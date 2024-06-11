f2768b4677c5d00e02c5448f15aa23938a5f9398
muhammedeminbekoz <m.eminbekoz19@gmail.com>
2024-06-11 15:14:44 +0300

    feat: Implement search functionality in Searchbar component

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -1,9 +1,31 @@
 import React from "react";
 
+
+
+
+
 class Searchbar extends React.Component {
+    state = {
+        searchQuery: ""
+    }
+    handleforSubmit = (e) => {
+        e.preventDefault()
+    }
     render() {
+
         return (
-            <h1>Searchbar</h1>
+            <form onSubmit={this.handleforSubmit}>
+                <div className="form-row mb-5">
+                    <div className="col-12">
+                        <input
+                            value={this.state.searchQuery}
+                            onChange={(event) => this.setState({ searchQuery: event.target.value })}
+                            type="text"
+                            className="form-control"
+                            placeholder="Search a movie" />
+                    </div>
+                </div>
+            </form>
         )
     }
 }

9cb5ca2d4f5f0acc789cbbea8f1f30e92ad7ddaa
muhammedeminbekoz <m.eminbekoz19@gmail.com>
2024-06-15 18:23:32 +0300

    refactor: Update Searchbar component to improve form layout and add "Add Movie" button

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -13,18 +13,20 @@ class Searchbar extends React.Component {
 
         return (
             <form onSubmit={this.handleforSubmit}>
-                <div className="form-row mb-5">
-                    <div className="col-12">
+                <div className="fform-row mb-5 d-flex align-items-center">
+                    <div className="col-10">
                         <input
-
                             onChange={this.props.searchMovieProp}
                             type="text"
                             className="form-control"
                             placeholder="Search a movie" />
 
                     </div>
+                    <div className="col-2">
+                        <button type="button" className="btn btn-primary ms-3">Add Movie</button>
+                    </div>
                 </div>
-            </form>
+            </form >
         )
     }
 }

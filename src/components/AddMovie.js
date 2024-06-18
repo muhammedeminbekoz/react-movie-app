0aa66f02880083110067eabf3f2d87987d52a924
muhammedeminbekoz <m.eminbekoz19@gmail.com>
2024-06-18 17:59:44 +0300

    refactor: Update AddMovie component to use functional component and hooks

--- src/components/AddMovie.js
+++ src/components/AddMovie.js
@@ -1,51 +1,48 @@
-import React from "react";
+import React from 'react';
+import serialize from 'form-serialize';
+import { useNavigate } from 'react-router-dom';
 
-class AddMovie extends React.Component {
+const AddMovie = ({ onAddMovie }) => {
+    const navigate = useNavigate();
 
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        const newMovie = serialize(e.target, { hash: true });
+        console.log(newMovie)
+        onAddMovie(newMovie);
+        navigate('/');
+    };
 
-    render() {
-
-        return (
-            <div className="container">
-                <form className="mt-5">
-                    <input className="form-control" id="disabledInput" type="text" placeholder="Fill The Form To Add A Movie.." disabled />
-                    <div className="form-row">
-                        <div className="form-group col-md-10">
-                            <label htmlFor="inputName">Name</label>
-                            <input type="text"
-                                className="form-control"
-                                name="name" />
-                        </div>
-                        <div className="form-group col-md-2">
-                            <label htmlFor="inputRating">Rating</label>
-                            <input
-                                type="text"
-                                className="form-control"
-                                name="rating" />
-                        </div>
+    return (
+        <div className="container">
+            <form className="mt-5" onSubmit={handleFormSubmit}>
+                <input className="form-control" id="disabledInput" type="text" placeholder="Fill The Form To Add A Movie.." disabled />
+                <div className="form-row">
+                    <div className="form-group col-10">
+                        <label htmlFor="inputName">Name</label>
+                        <input type="text" className="form-control" name="name" />
                     </div>
-                    <div className="form-row">
-                        <div className="form-group col-md-12">
-                            <label htmlFor="inputImage">Image URL</label>
-                            <input
-                                type="text"
-                                className="form-control"
-                                name="imageURL" />
-                        </div>
+                    <div className="form-group col-2">
+                        <label htmlFor="inputRating">Rating</label>
+                        <input type="text" className="form-control" name="rating" />
                     </div>
-                    <div className="form-row">
-                        <div className="form-group col-md-12">
-                            <label htmlFor="overviewTextarea">Overview</label>
-                            <textarea
-                                className="form-control"
-                                name="overview" rows="5"></textarea>
-                        </div>
+                </div>
+                <div className="form-row">
+                    <div className="form-group col-12">
+                        <label htmlFor="inputImage">Image URL</label>
+                        <input type="text" className="form-control" name="imageURL" />
                     </div>
-                    <input type="submit" className="btn btn-danger btn-block" value="Add Movie" />
-                </form>
-            </div>
-        )
-    }
-}
+                </div>
+                <div className="form-row">
+                    <div className="form-group col-md-12">
+                        <label htmlFor="overviewTextarea">Overview</label>
+                        <textarea className="form-control" name="overview" rows="5"></textarea>
+                    </div>
+                </div>
+                <input type="submit" className="btn btn-danger btn-block" value="Add Movie" />
+            </form>
+        </div>
+    );
+};
 
-export default AddMovie
+export default AddMovie;
